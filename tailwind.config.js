25fa2b9368c5e741d9719ee2925ff9740cec2925
pandeYYsushant <pandeyysushant@gmail.com>
2024-10-21 20:46:10 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        'dark-gray': '#1E1E1E',
+        'light-gray': '#2E2E2E',
+      }
+    },
+  },
+  plugins: [],
+}
+
